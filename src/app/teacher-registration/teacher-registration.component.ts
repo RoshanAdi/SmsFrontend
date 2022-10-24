b13c0c05c81ea3065034189dbefe325451ff925f
Roshan <roshanmahesh1@gmail.com>
2022-10-24 21:54:23 +0530

    Teacher registration

---
+++ src/app/teacher-registration/teacher-registration.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-teacher-registration',
+  templateUrl: './teacher-registration.component.html',
+  styleUrls: ['./teacher-registration.component.css']
+})
+export class TeacherRegistrationComponent implements OnInit {
+
+  StudentAge: string | undefined
+  status: boolean | undefined;
+
+
+  constructor(private http:HttpClient,private router: Router, ) { }
+
+  ngOnInit(): void {
+  }
+
+  VerifyPasswords() {
+    // @ts-ignore
+    if (document.getElementById('StudentPassword').value == document.getElementById('RepeatPw').value) {
+      // @ts-ignore
+      document.getElementById('message').style.color = 'green';
+      // @ts-ignore
+      document.getElementById('message').innerHTML = '  Matching';
+      this.status = false
+    } else {
+      // @ts-ignore
+      document.getElementById('message').style.color = 'red';
+      // @ts-ignore
+      document.getElementById('message').innerHTML = '  Not matching';
+      this.status = true
+    }
+  }
+
+
+  Submit(TeacherRegData: NgForm) {
+    console.log(TeacherRegData)                                            /// delete
+    this.http.post('http://localhost:8089/teacher/register', TeacherRegData)
+      .subscribe((result) => {
+        console.warn("result", result)                ////remove
+      })
+    this.router.navigate(['/verify']);
+
+
+  }
+}
+
+

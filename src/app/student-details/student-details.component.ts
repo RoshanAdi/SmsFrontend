bf24ca54acb6174861e80eba47af4cf08e5e789d
Roshan <roshanmahesh1@gmail.com>
2022-10-26 09:35:09 +0530

    Teacher profile

--- src/app/student-details/student-details.component.ts
+++ src/app/student-details/student-details.component.ts
@@ -6,7 +6,9 @@ import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
 import {Router} from "@angular/router";
 import {NgForm} from "@angular/forms";
-
+import {TokenStorageService} from "../_services/token-storage.service";
+import jwt_decode from "jwt-decode";
+const USER_KEY = 'auth-user';
 
 @Component({
   selector: 'app-student-details',
@@ -16,7 +18,7 @@ import {NgForm} from "@angular/forms";
 export class StudentDetailsComponent implements OnInit {
 
 
-constructor( private http:HttpClient,private userNameService: UsernameService, private router: Router,){}
+constructor( private tokenStorage: TokenStorageService,private http:HttpClient,private userNameService: UsernameService, private router: Router,){}
   myObj:any
 name:any
   public firstName: String | undefined;
@@ -27,16 +29,25 @@ name:any
   public birthDate: String | undefined;
   public address: String | undefined;
   public tp: String | undefined;
+  public nIc: String | undefined;
+  public role: String | undefined;
   public  ShowInputFields = false
   public  HideDetails = true
-
+public StudentUser = false
+  public TeacherUser = false
+  currentUser: any;
 
 
 
   ngOnInit(): void { console.log("printing the username found from token = "+this.userNameService.getUserName())
-const username = this.userNameService.getUserName()
+    const user = window.sessionStorage.getItem(USER_KEY);
+    this.currentUser = jwt_decode(String(user));
+    let Role = String(this.currentUser.roles)
+        console.warn("role found on token = "+Role.slice(5, ))
+
+    const username = this.userNameService.getUserName()
     this.http
-      .get("http://localhost:8089/student/"+username)
+      .get("http://localhost:8089/"+Role.slice(5, )+"/"+username)
       .subscribe(response=> {
               this.name = JSON.stringify(response);
          this.myObj = JSON.parse(this.name);
@@ -44,13 +55,20 @@ const username = this.userNameService.getUserName()
         this.fullName = this.myObj.fullName
         this.lastName = this.myObj.lastName
         this.username = this.myObj.username
+        this.role = this.myObj.role
         this.birthDate = this.myObj.birthDate
         this.tp = this.myObj.tp
         this.address = this.myObj.address
         this.age = this.myObj.age
+        this.nIc = this.myObj.nIc
               console.log("Printing student = "+this.myObj.firstName)    });
 
-
+if (Role.slice(5, )=='Student'){
+  this.StudentUser = true;
+}
+    if (Role.slice(5, )=='Teacher'){
+      this.TeacherUser = true;
+    }
 
 
   }
@@ -60,8 +78,10 @@ const username = this.userNameService.getUserName()
     this.HideDetails = false;
   }
   Update(UpdateData: NgForm) {
-    //console.log("Modified = "+UpdateData.firstName)
-    this.http.put('http://localhost:8089/student/'+this.username, UpdateData)
+    const user = window.sessionStorage.getItem(USER_KEY);
+    this.currentUser = jwt_decode(String(user));
+    let Role = String(this.currentUser.roles)
+    this.http.put('http://localhost:8089/'+Role.slice(5, )+'/'+this.username, UpdateData)
 
   .subscribe((result) => {
     console.warn("result", result)                ////remove
